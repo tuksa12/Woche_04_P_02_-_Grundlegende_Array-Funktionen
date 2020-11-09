1248ffb47953f4e63a17cce86110c4a4970ebf67
Martin Dunker <ga27kin@mytum.de>
2020-11-09 22:03:13 +0100

    Exercise-Template pushed by Artemis

---
+++ src/pgdp/arrays/Arrays.java
@@ -0,0 +1,28 @@
+package pgdp.arrays;
+
+import static pgdp.MiniJava.*;
+
+public class Arrays {
+    public static void print(int[] feld) {
+        // TODO
+    }
+
+    public static void minUndMax(int[] feld) {
+        // TODO
+    }
+
+    public static int[] invertieren(int[] feld) {
+        // TODO
+        return null;
+    }
+
+    public static int[] schneiden(int[] feld, int laenge) {
+        // TODO
+        return null;
+    }
+
+    public static int[] linearisieren(int[][] feld) {
+        // TODO
+        return null;
+    }
+}
